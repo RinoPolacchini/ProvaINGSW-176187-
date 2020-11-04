fa05bc332881bf4181ea179ab975c9a9183c37b5
Rino <Rino@DESKTOP-5RFD7SU.homenet.telecomitalia.it>
2020-11-04 12:56:02 +0100

    First Commit

---
+++ ProvaIntermedia/src/main/java/AssigmentINGSW/ProvaIntermedia/App.java
@@ -0,0 +1,13 @@
+package AssigmentINGSW.ProvaIntermedia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
